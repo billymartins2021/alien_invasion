fba9838072a9660e78ffb06115a99193ff303825
billyhawes <workinprogress@sonic.net>
2021-03-23 17:22:36 -0700

    Started using GIT for version control after completing work on the spaceship and bullets. Aliens will come next.

--- alien.py
+++ alien.py
@@ -18,4 +18,4 @@ class Alien(Sprite):
         self.rect.y = self.rect.height
 
         # Store the alien's exact horizontal position.
-        self.x= float(self.rect.x)
+        self.x = float(self.rect.x)
