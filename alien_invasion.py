9a67aba52d2115a60cc551ce751f5b331bca8c68
billyhawes <workinprogress@sonic.net>
2021-03-27 20:55:53 -0700

    Added some functionality in Settings to handle some of the more dynamic settings.

--- alien_invasion.py
+++ alien_invasion.py
@@ -70,6 +70,7 @@ class AlienInvasion:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _check_events(self):
         # Respond to keypresses and mouse events.
@@ -114,6 +115,8 @@ class AlienInvasion:
         """Start a new game when the player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
             self._start_game()
 
     def _create_alien(self, alien_number, row_number):
